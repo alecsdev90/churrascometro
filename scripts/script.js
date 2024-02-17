2d155c115049f466b37e81ef105a00fb9cb66d6c
alecsdev90 <alecostasan.dev@gmail.com>
2024-02-17 14:43:46 -0300

    atualização de layout e responsividade

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,10 @@
 // refrigerante - 1000ml por pessoa + de 6h - 1500ml    
 // criança vale por 0,5 
 
+var nome = prompt("Qual é o seu nome?");
+var tipoCarne = prompt("Quais são os tipos de carnes que você gosta em um churrasco? Ex: bovina, suína, linguiça e frango");
+var tipoAcompanhamento = prompt("Ultima pergunta: Quais os acompanhamentos não podem faltar? Ex: arroz, farofa, vinagrete, maionese e saladas");
+
 let inputHomens = document.getElementById("homens");
 let inputMulheres = document.getElementById("mulheres");
 let inputCriancas = document.getElementById("criancas");
@@ -27,10 +31,9 @@ function calcular(){
     let qtdTotalBebidasM = bebidasPP (duracao)/2 * mulheres; 
     let qtdTotalBebidasC = bebidasPP (duracao)/2 * criancas; 
 
-
-    resultado.innerHTML = `<p>Para essa quantidade de pessoas será necessário comprar ${Math.ceil(qtdTotalCarneH/1000 + qtdTotalCarneM/1000 + qtdTotalCarneC/1000)}kg de carne podendo variar entre carne, linguiça e frango. Poderá também servir acompanhamentos tradicionais para churrasco como arroz, farofa, vinagrete, maionese de batatas e saladas variadas.</p>`
-    resultado.innerHTML += `<p>Se desejar servir bebidas alcoólicas você poderá comprar ${Math.ceil(qtdTotalCervejaH/355 + qtdTotalCervejaM/355)} latas de cerveja para os adultos.</p>`
-    resultado.innerHTML += `<p>Com relação as demais bebidas poderá comprar ${Math.ceil(qtdTotalBebidasH/2000 + qtdTotalBebidasM/2000 + qtdTotalBebidasC/2000)} pet's 2L de refrigerantes, podendo fazer a troca de parte dessa quantidade por água mineral e sucos.</p>`
+    resultado.innerHTML = `<p>Olá ${nome}!</p>` 
+    resultado.innerHTML += `<p>Para essa quantidade de pessoas será necessário comprar ${Math.ceil(qtdTotalCarneH/1000 + qtdTotalCarneM/1000 + qtdTotalCarneC/1000)}kg de carnes podendo variar entre ${tipoCarne}. Poderá também servir acompanhamentos tradicionais para churrasco como ${tipoAcompanhamento}.</p>`
+    resultado.innerHTML += `<p>Se desejar servir bebidas alcoólicas você poderá comprar ${Math.ceil(qtdTotalCervejaH/355 + qtdTotalCervejaM/355)} latas de cerveja para os adultos. Com relação as demais bebidas poderá comprar ${Math.ceil(qtdTotalBebidasH/2000 + qtdTotalBebidasM/2000 + qtdTotalBebidasC/2000)} pet's 2L de refrigerantes, podendo fazer a troca de parte dessa quantidade por água mineral e sucos.</p>`
 }
 
 
